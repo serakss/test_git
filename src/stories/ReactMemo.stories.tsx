55bebff1a79ea6e77d7f93e3ae6823bc21c64c6f
serakss <serak_96@mail.ru>
2021-11-09 20:11:33 +0300

    new stories

--- src/stories/ReactMemo.stories.tsx
+++ src/stories/ReactMemo.stories.tsx
@@ -1,18 +1,24 @@
+import React, {useState} from "react";
+
 export default {
-    title:"React.memo demo"
+    title: "React.memo demo"
 }
 
-const NewMessagesCounter = (props:{count: number})=>{
-return <div>{props.count}</div>
+const NewMessagesCounter = (props: { count: number }) => {
+    return <div>{props.count}</div>
 }
 
-const User =(props:{user:Array<string>})=>{
-    return <div>{props.user.map((u,i)=><div>{u}</div>)}</div>
+const UsersSecret = (props: { user: Array<string> }) => {
+    console.log("USER")
+    return <div>{props.user.map((u, i) => <div key={i}>{u}</div>)}</div>
 }
-
-export const Example1 = ()=>{
+const User = React.memo(UsersSecret);
+export const Example1 = () => {
+    const [counter, setCounter] = useState(0);
+    const [users, setUsers] = useState(["Dimych", "Valera", "Artem"]);
     return <>
-    <NewMessagesCounter count={10}/>
-        <User user={["Dimych","Valera","Artem"]}/>
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+        <NewMessagesCounter count={counter}/>
+        <User user={users}/>
     </>
 }
