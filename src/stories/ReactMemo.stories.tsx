ce9097dcddc3dddcf5ce6ba2ff9213371b07ac90
serakss <serak_96@mail.ru>
2021-11-09 19:47:48 +0300

    new stories

--- src/stories/ReactMemo.stories.tsx
+++ src/stories/ReactMemo.stories.tsx
@@ -2,6 +2,17 @@ export default {
     title:"React.memo demo"
 }
 
-const NewMessagesCounter = ()=>{
+const NewMessagesCounter = (props:{count: number})=>{
+return <div>{props.count}</div>
+}
 
+const User =(props:{user:Array<string>})=>{
+    return <div>{props.user.map((u,i)=><div>{u}</div>)}</div>
+}
+
+export const Example1 = ()=>{
+    return <>
+    <NewMessagesCounter count={10}/>
+        <User user={["Dimych","Valera","Artem"]}/>
+    </>
 }
